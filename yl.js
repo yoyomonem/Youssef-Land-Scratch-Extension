927d92a0c52a09e0d9b052866452471e5acbeefa
Y Nasr <yoyo.monem22@gmail.com>
2022-11-01 21:21:57 +0100

    Do 2 things

--- yl.js
+++ yl.js
@@ -14,7 +14,7 @@ class ScratchYL {
                     "text": "when the green flag is clicked"
                 },
                 {
-                    "opcode": "sayTextForSecs",
+                    "opcode": "sayTextForSecs({{text}}, {{secs}})",
                     "blockType": "command",
                     "text": "say [text] for (secs) secs",
                     "arguments": {
