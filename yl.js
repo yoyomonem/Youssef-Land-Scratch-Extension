7fb1156f495ac7146f53c2204d50052becec5faf
Y Nasr <yoyo.monem22@gmail.com>
2022-11-21 17:38:42 +0100

    Do 2 things

--- yl.js
+++ yl.js
@@ -1,20 +1,21 @@
-import { Project } from "https://unpkg.com/leopard@^1/dist/index.esm.js";
+import Project from "https://unpkg.com/leopard@^1/dist/index.esm.js";
+import "./types/Scratch";
 class ScratchYL {
-    constructor(){
+    constructor() {
         /* nothing to add here */
     }
-    getData(){
+    getData() {
         return {
             "id": "YL-1-0",
             "name": "Youssef Land for Scratch (extension version 1.0)",
             "blocks": [
                 {
-                    "opcode": "whenFlagClicked",
+                    "opcode": "whenGreenFlag",
                     "blockType": "hat",
                     "text": "when the green flag is clicked"
                 },
                 {
-                    "opcode": "sayTextForSecs("+text+", "+secs+")",
+                    "opcode": "sayTextForSecs("+arguments.text+", "+arguments.secs+")",
                     "blockType": "command",
                     "text": "say [text] for (secs) secs",
                     "arguments": {
